c1d9acdea39a0c60cebdbc97d8f65ea1a41a6862
Samuel Osei Kwakye <kwakyesamuelosei@gmail.com>
2019-11-20 16:00:07 +0000

    Travis

--- src/main/java/io/turntabl/springwebservices/Controller/MathsController.java
+++ src/main/java/io/turntabl/springwebservices/Controller/MathsController.java
@@ -1,17 +1,21 @@
 package io.turntabl.springwebservices.Controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.turntabl.springwebservices.Transfers.Number;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class MathsController {
 
     @Autowired
     private Maths maths;
 
+    @ApiOperation("Add Two Ints")
     @GetMapping("/maths/add")
     public Number add(@RequestParam(name = "num_1",defaultValue = "1") Integer num_1, @RequestParam(name = "num_2",defaultValue = "1") Integer num_2){
         Integer total = maths.add(num_1,num_2);
@@ -20,6 +24,7 @@ public class MathsController {
         return number;
     }
 
+    @ApiOperation("Subtracting Two Ints")
     @GetMapping("/maths/subtract")
     public Number subtract(@RequestParam(name = "num_1",defaultValue = "1") Integer num_1, @RequestParam(name = "num_2",defaultValue = "1") Integer num_2){
         Integer total = maths.subtract(num_1,num_2);
